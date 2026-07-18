960a8472954fda379a07e20c64edfe03abcb09f6
Bhanusnehitha <bhanusnehitha13@gmail.com>
2026-07-18 19:49:29 +0530

    Delete recognition directory

--- recognition/test_camera.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-for i in range(5):
-    cap = cv2.VideoCapture(i, cv2.CAP_DSHOW)
-    if cap.isOpened():
-        print(f"Camera index {i} is available")
-        cap.release()
-    else:
-        print(f"Camera index {i} not available")
